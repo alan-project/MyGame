86e64b73e60ffee675e70c9253d491abf46604d5
alan-project <86268065+alan-project@users.noreply.github.com>
2022-02-13 18:11:02 -0500

    Added viewModel and linked with view (#6)

--- app/src/main/java/net/alanproject/mygame/ui/detail/DetailFragment.kt
+++ app/src/main/java/net/alanproject/mygame/ui/detail/DetailFragment.kt
@@ -6,14 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import net.alanproject.mygame.R
 import net.alanproject.mygame.databinding.FragmentDetailBinding
-
-
 
 class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailBinding
+    private val viewModel: DetailViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +23,7 @@ class DetailFragment : Fragment() {
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
+        binding.vm = viewModel
         return binding.root
 
     }
---
+++ app/src/main/java/net/alanproject/mygame/ui/home/HomeViewModel.kt
@@ -0,0 +1,6 @@
+package net.alanproject.mygame.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel:ViewModel() {
+}
