03cfa5a7c7308d6a39ed3392bf425456ad4123b3
alan-project <86268065+alan-project@users.noreply.github.com>
2022-02-13 00:10:32 -0500

    applied Timber for logging (#2)

--- app/src/main/java/net/alanproject/mygame/ui/MainActivity.kt
+++ app/src/main/java/net/alanproject/mygame/ui/MainActivity.kt
@@ -1,11 +1,14 @@
-package net.alanproject.mygame
+package net.alanproject.mygame.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.alanproject.mygame.R
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
