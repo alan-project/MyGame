775fc87d2abf46cd9183cd9e9d6121f12f425a66
alan-project <banana205@gmail.com>
2022-02-13 01:12:27 -0500

    added default fragments

---
+++ app/src/main/java/net/alanproject/mygame/ui/list/ListFragment.kt
@@ -0,0 +1,17 @@
+package net.alanproject.mygame.ui.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ListFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
