58b39e9ac1821adaabb3b5339f73a81cf7e31d70
alan-project <banana205@gmail.com>
2022-02-13 01:22:24 -0500

    added fragment

--- app/src/main/java/net/alanproject/mygame/ui/detail/DetailFragment.kt
+++ app/src/main/java/net/alanproject/mygame/ui/detail/DetailFragment.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import net.alanproject.mygame.R
+import net.alanproject.mygame.databinding.FragmentDetailBinding
 
-class DetailFragment:Fragment() {
+
+
+class DetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return binding.root
 
     }
 }
--- app/src/main/java/net/alanproject/mygame/ui/list/ListFragment.kt
+++ app/src/main/java/net/alanproject/mygame/ui/list/ListFragment.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import net.alanproject.mygame.R
+import net.alanproject.mygame.databinding.FragmentListBinding
 
-class ListFragment:Fragment() {
+class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false)
+        return binding.root
     }
 }
