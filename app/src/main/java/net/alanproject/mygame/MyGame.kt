a24d1a9959f067691d7d46e5196665db3274f4db
alan-project <banana205@gmail.com>
2022-02-13 00:10:05 -0500

    applied Timber for logging

---
+++ app/src/main/java/net/alanproject/mygame/MyGame.kt
@@ -0,0 +1,11 @@
+package net.alanproject.mygame
+
+import android.app.Application
+import timber.log.Timber
+
+class MyGame:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
