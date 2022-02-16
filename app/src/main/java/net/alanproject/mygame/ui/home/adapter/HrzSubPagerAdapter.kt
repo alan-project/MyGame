1fbf1bf4db1d3cc4a6e23412fff014829301e337
alan-project <banana205@gmail.com>
2022-02-15 23:07:28 -0500

    applied preview in viewPager2

--- app/src/main/java/net/alanproject/mygame/ui/home/adapter/HrzSubPagerAdapter.kt
+++ app/src/main/java/net/alanproject/mygame/ui/home/adapter/HrzSubPagerAdapter.kt
@@ -2,36 +2,35 @@ package net.alanproject.mygame.ui.home.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import net.alanproject.domain.model.response.games.Result
 import net.alanproject.mygame.R
-import net.alanproject.mygame.databinding.ItemTopBannerBinding
+import net.alanproject.mygame.databinding.ItemHrzFullBinding
+import net.alanproject.mygame.databinding.ItemHrzSubBinding
 
-class TopBannerAdapter(private val games: List<Result>) :
-    RecyclerView.Adapter<TopBannerAdapter.TopBannerViewHolder>() {
+class HrzSubPagerAdapter(private val games: List<Result>) :
+    RecyclerView.Adapter<HrzSubPagerAdapter.HrzSubViewHolder>() {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopBannerViewHolder =
-        TopBannerViewHolder(
-            DataBindingUtil.inflate<ItemTopBannerBinding>(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HrzSubViewHolder =
+        HrzSubViewHolder(
+            DataBindingUtil.inflate<ItemHrzSubBinding>(
                 LayoutInflater.from(parent.context),
-                R.layout.item_top_banner, parent, false
+                R.layout.item_hrz_sub, parent, false
             )
         )
 
 
-    override fun onBindViewHolder(holder: TopBannerViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: HrzSubViewHolder, position: Int) {
         val actualPosition = position % games.size
         holder.bind(games[actualPosition])
     }
 
     override fun getItemCount(): Int = Int.MAX_VALUE
 
-    class TopBannerViewHolder(
-        private val binding: ItemTopBannerBinding
+    class HrzSubViewHolder(
+        private val binding: ItemHrzSubBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(game: Result) {
