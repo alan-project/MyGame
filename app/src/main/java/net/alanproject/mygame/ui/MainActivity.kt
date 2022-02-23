cefe387cb86f11a57d0a4e5fdf5e73e8fefd4d29
alan-project <banana205@gmail.com>
2022-02-22 23:03:17 -0500

    modified navigation visibility for detail page

--- app/src/main/java/net/alanproject/mygame/ui/MainActivity.kt
+++ app/src/main/java/net/alanproject/mygame/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package net.alanproject.mygame.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
@@ -26,5 +27,20 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
         binding.navi.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.detailFragment -> hideBottomNav()
+                else -> showBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        binding.navi.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        binding.navi.visibility = View.GONE
     }
 }
